1e21aaa970def61a499b3264825e1908c2a0a885
Tiffany Kalin <tkalin@untangle.com>
2020-12-22 11:46:44 -0700

    MFW-1178: Scaffolding to being adding protobuff to restd

--- services/gind/gind.go
+++ services/gind/gind.go
@@ -28,104 +28,8 @@ func Startup() {
 	engine.Use(gin.Recovery())
 	engine.Use(addHeaders)
 
-	// Allow cross-site for dev - this should be disabled in production
-	// config := cors.DefaultConfig()
-	// config.AllowAllOrigins = true
-	// engine.Use(cors.New(config))
+	engine.GET("/test", statusSessions)
 
-	// A server-side store would be better IMO, but I can't find one.
-	// -dmorris
-	/*
-	TODO
-	store := cookie.NewStore([]byte(GenerateRandomString(32)))
-	// store := cookie.NewStore([]byte("secret"))
-
-	engine.Use(sessions.Sessions("auth_session", store))
-	engine.Use(addTokenToSession)
-	*/
-
-	/*
-	TODO
-	engine.GET("/", rootHandler)
-
-	engine.GET("/ping", pingHandler)
-
-	engine.POST("/account/login", authRequired())
-	engine.POST("/account/logout", authLogout)
-	engine.GET("/account/logout", authLogout)
-	engine.GET("/account/status", authStatus)
-
-	api := engine.Group("/api")
-	api.Use(authRequired())
-
-	api.GET("/settings", getSettings)
-	api.GET("/settings/*path", getSettings)
-	api.POST("/settings", setSettings)
-	api.POST("/settings/*path", setSettings)
-	api.DELETE("/settings", trimSettings)
-	api.DELETE("/settings/*path", trimSettings)
-
-	api.GET("/logging/:logtype", getLogOutput)
-
-	api.GET("/defaults", getDefaultSettings)
-	api.GET("/defaults/*path", getDefaultSettings)
-
-	api.POST("/reports/create_query", reportsCreateQuery)
-	api.GET("/reports/get_data/:query_id", reportsGetData)
-	api.POST("/reports/close_query/:query_id", reportsCloseQuery)
-
-	api.POST("/warehouse/capture", warehouseCapture)
-	api.POST("/warehouse/close", warehouseClose)
-	api.POST("/warehouse/playback", warehousePlayback)
-	api.POST("/warehouse/cleanup", warehouseCleanup)
-	api.GET("/warehouse/status", warehouseStatus)
-	api.POST("/control/traffic", trafficControl)
-
-	api.POST("/netspace/request", netspaceRequest)
-
-	api.GET("/status/sessions", statusSessions)
-	api.GET("/status/system", statusSystem)
-	api.GET("/status/hardware", statusHardware)
-	api.GET("/status/upgrade", statusUpgradeAvailable)
-	api.GET("/status/build", statusBuild)
-	api.GET("/status/license", statusLicense)
-	api.GET("/status/wantest/:device", statusWANTest)
-	api.GET("/status/uid", statusUID)
-	api.GET("/status/command/find_account", statusCommandFindAccount)
-	api.GET("/status/interfaces/:device", statusInterfaces)
-	api.GET("/status/arp/", statusArp)
-	api.GET("/status/arp/:device", statusArp)
-	api.GET("/status/dhcp", statusDHCP)
-	api.GET("/status/route", statusRoute)
-	api.GET("/status/routetables", statusRouteTables)
-	api.GET("/status/route/:table", statusRoute)
-	api.GET("/status/rules", statusRules)
-	api.GET("/status/routerules", statusRouteRules)
-	api.GET("/status/wwan/:device", statusWwan)
-	api.GET("/status/wifichannels/:device", statusWifiChannels)
-	api.GET("/status/wifimodelist/:device", statusWifiModelist)
-
-	api.GET("/wireguard/keypair", wireguardKeyPair)
-	api.POST("/wireguard/publickey", wireguardPublicKey)
-
-	api.GET("/classify/applications", getClassifyAppTable)
-	api.GET("/classify/categories", getClassifyCatTable)
-
-	api.GET("/logger/:source", loggerHandler)
-	api.GET("/debug", debugHandler)
-	api.POST("/gc", gcHandler)
-
-	api.POST("/fetch-licenses", fetchLicensesHandler)
-	api.POST("/factory-reset", factoryResetHandler)
-	api.POST("/sysupgrade", sysupgradeHandler)
-	api.POST("/upgrade", upgradeHandler)
-
-	api.POST("/reboot", rebootHandler)
-	api.POST("/shutdown", shutdownHandler)
-
-	api.POST("/releasedhcp/:device", releaseDhcp)
-	api.POST("/renewdhcp/:device", renewDhcp)
-	*/
 	// files
 	engine.Static("/admin", "/www/admin")
 	engine.Static("/settings", "/www/settings")
@@ -135,33 +39,10 @@ func Startup() {
 	// handle 404 routes
 	engine.NoRoute(noRouteHandler)
 
-	/*
-	TODO
-	prof := engine.Group("/pprof")
-	prof.Use(authRequired())
-
-	prof.GET("/", pprofHandler(pprof.Index))
-	prof.GET("/cmdline", pprofHandler(pprof.Cmdline))
-	prof.GET("/profile", pprofHandler(pprof.Profile))
-	prof.POST("/symbol", pprofHandler(pprof.Symbol))
-	prof.GET("/symbol", pprofHandler(pprof.Symbol))
-	prof.GET("/trace", pprofHandler(pprof.Trace))
-	prof.GET("/block", pprofHandler(pprof.Handler("block").ServeHTTP))
-	prof.GET("/goroutine", pprofHandler(pprof.Handler("goroutine").ServeHTTP))
-	prof.GET("/heap", pprofHandler(pprof.Handler("heap").ServeHTTP))
-	prof.GET("/mutex", pprofHandler(pprof.Handler("mutex").ServeHTTP))
-	prof.GET("/threadcreate", pprofHandler(pprof.Handler("threadcreate").ServeHTTP))
-	*/
-
 	// listen and serve on 0.0.0.0:80
 	// TODO change to :80 once take out packetd restd
 	go engine.Run(":8080")
 
-	/*
-	TODO
-	cert, key := certmanager.GetConfiguredCert()
-	go engine.RunTLS(":443", cert, key)
-	*/
 	logger.Info("The RestD engine has been started\n")
 	
 
@@ -207,24 +88,3 @@ func ginlogger() gin.HandlerFunc {
 		c.Next()
 	}
 }
-
-// addTokenToSession checks for a "token" argument, and adds it to the session
-// this is easier than passing it around among redirects
-/*
-TODO
-func addTokenToSession(c *gin.Context) {
-	token := c.Query("token")
-	if token == "" {
-		return
-	}
-	// TODO logger.info
-	session := sessions.Default(c)
-	session.Set("token", token)
-	err := session.Save()
-	if err != nil {
-		// TODO 
-		fmt.Println(err)
-	}
-	authRequired()
-}
-*/
---
+++ services/gind/sessions.go
@@ -0,0 +1,167 @@
+package gind
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jsommerville-untangle/golang-shared/services/logger" 
+)
+
+// statusSessions is the RESTD /api/status/sessions handler
+func statusSessions(c *gin.Context) {
+	logger.Debug("statusSession()\n")
+
+	sessions, err := getSessions()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	c.JSON(http.StatusOK, sessions)
+}
+
+func getSessions() ([]map[string]interface{}, error) {
+	var sessions []map[string]interface{}
+	m1 := make(map[string]interface{})
+	m1["ping"] = "pong"
+	m1["tennis"] = "ball"
+	sessions = append(sessions, m1)
+	m2 := make(map[string]interface{})
+	m2["pong"] = "ping"
+	m2["ball"] = "tennis"
+	sessions = append(sessions, m2)
+	return sessions, nil
+}
+
+// getSessions returns the fully merged list of sessions
+// as a list of map[string]interface{}
+// It reads the session list from /proc/net/nf_conntrack
+// and merges in the values for each session in dict
+/*func getSessions() ([]map[string]interface{}, error) {
+	var sessions []map[string]interface{}
+
+	conntrackTable := dispatch.GetConntrackTable()
+
+	for _, v := range conntrackTable {
+		m := parseConntrack(v)
+		if m != nil {
+			sessions = append(sessions, m)
+		}
+	}
+
+	// build a tuple map to store the sessions
+	tupleMap := make(map[uint32]map[string]interface{})
+	for _, s := range sessions {
+		var ctidx interface{} = s["conntrack_id"]
+		ctid, ok := ctidx.(uint32)
+		if !ok {
+			logger.Warn("Invalid conntrack_id type: %T\n", ctidx)
+			continue
+		}
+		if ctid == 0 {
+			logger.Warn("Invalid conntrack_id: %d\n", ctid)
+			continue
+		}
+		tupleMap[ctid] = s
+	}
+
+	// read the data from dict "sessions" table
+	/* sessionTable, err := dict.GetSessions()
+	if err != nil {
+		logger.Warn("Unable to get sessions: %v\n", err)
+	}
+
+	// for all the data in the dict "sessions" table, merge it into the matching tupleMap entries
+	for ctid, session := range sessionTable {
+		_, ok := tupleMap[ctid]
+		if !ok {
+			// matching session not found, continue
+			continue
+		}
+
+		// matching session found, merge the (new) values into the map
+		for key, value := range session {
+			_, alreadyFound := tupleMap[ctid][key]
+			if alreadyFound {
+				continue
+			} else {
+				tupleMap[ctid][key] = value
+			}
+		}
+	}
+
+	// return all the merged values from the tupleMap
+	var sessionList []map[string]interface{}
+	for _, v := range tupleMap {
+		sessionList = append(sessionList, v)
+	}
+
+	return sessionList, nil
+}
+
+// parse a line of /proc/net/nf_conntrack and return the info in a map
+func parseConntrack(ct *dispatch.Conntrack) map[string]interface{} {
+	m := make(map[string]interface{})
+
+	// ignore 127.0.0.1 traffic
+	if ct.ClientSideTuple.ClientAddress != nil && ct.ClientSideTuple.ClientAddress.String() == "127.0.0.1" {
+		return nil
+	}
+	if ct.ClientSideTuple.ServerAddress != nil && ct.ClientSideTuple.ServerAddress.String() == "127.0.0.1" {
+		return nil
+	}
+	if ct.ClientSideTuple.ClientAddress != nil && ct.ClientSideTuple.ClientAddress.String() == "::1" {
+		return nil
+	}
+	if ct.ClientSideTuple.ServerAddress != nil && ct.ClientSideTuple.ServerAddress.String() == "::1" {
+		return nil
+	}
+
+	m["conntrack_id"] = ct.ConntrackID
+	//m["session_id"] = ct.SessionID
+	//m["family"] = ct.Family
+	//m["ip_protocol"] = ct.ClientSideTuple.Protocol
+
+	//m["timeout_seconds"] = ct.TimeoutSeconds
+	//m["tcp_state"] = ct.TCPState
+
+	//m["client_address"] = ct.ClientSideTuple.ClientAddress
+	//m["client_port"] = ct.ClientSideTuple.ClientPort
+	//m["server_address"] = ct.ClientSideTuple.ServerAddress
+	//m["server_port"] = ct.ClientSideTuple.ServerPort
+	//m["client_address_new"] = ct.ServerSideTuple.ClientAddress
+	//m["client_port_new"] = ct.ServerSideTuple.ClientPort
+	//m["server_address_new"] = ct.ServerSideTuple.ServerAddress
+	//m["server_port_new"] = ct.ServerSideTuple.ServerPort
+
+	//m["bytes"] = ct.TotalBytes
+	//m["client_bytes"] = ct.ClientBytes
+	//m["server_bytes"] = ct.ServerBytes
+	//m["packets"] = ct.TotalPackets
+	//m["client_packets"] = ct.ClientPackets
+	//m["server_packets"] = ct.ServerPackets
+
+	m["timestamp_start"] = ct.TimestampStart
+	if ct.TimestampStart != 0 {
+		m["age_milliseconds"] = (uint64(time.Now().UnixNano()) - ct.TimestampStart) / 1000000
+	}
+
+	m["timestamp_stop"] = ct.TimestampStop
+
+
+	//var mark uint32
+	//mark = ct.ConnMark
+	//clientInterfaceID := mark & 0x000000ff
+	//clientInterfaceType := mark & 0x03000000 >> 24
+	//serverInterfaceID := mark & 0x0000ff00 >> 8
+	//serverInterfaceType := mark & 0x0c000000 >> 26
+	//priority := mark & 0x00ff0000 >> 16
+	//m["mark"] = mark
+	//m["client_interface_id"] = clientInterfaceID
+	//m["client_interface_type"] = clientInterfaceType
+	//m["server_interface_id"] = serverInterfaceID
+	//m["server_interface_type"] = serverInterfaceType
+	//m["priority"] = priority
+
+	return m
+}*/
